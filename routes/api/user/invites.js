6f9b78f42ce7381411c9a3663d958460abd20f2f
tomaszsojka <tomek.s@windowslive.com>
2020-12-13 22:19:11 +0100

    Request for showing invites of logged in user working, sending invites from the team only to not members

--- routes/api/user/invites.js
+++ routes/api/user/invites.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const User = require('../../../models/User');
+const UserSession = require('../../../models/UserSession');
+const Team = require('../../../models/Team');
 const router = express.Router();
 
 router.post('/send-invite', (req, res) => {
@@ -10,25 +12,109 @@ router.post('/send-invite', (req, res) => {
         email 
     } = body;
 
-  User.findOneAndUpdate({
-    email : email  
-  }, {
-    $addToSet: {
-      teamInvites: teamId
-    }
-  }, null, (err, user) => {
+  Team.find({
+    _id : teamId
+  }, (err, teams) => {
     if(err) {
       return res.send({
         success : false,
         message : "Error: Server error"
       });
+    } else if(teams.length !== 1) {
+      return res.send({
+        success : false,
+        message : 'Error : Invalid'
+        });
     } else {
-        return res.send({
-            success : true,
-            message : "Invitation to the team sent"
-          });
+      User.find({
+        email : email
+      }, (err, users) => {
+        if(err) {
+            return res.send({
+              success : false,
+              message : "Error: Server error"
+            });
+          } else if(users.length === 0) {
+              return res.send({
+                success : false,
+                message : "No user found with that email"
+              });
+          } else if(teams[0].playersIds.includes(users[0]._id)) {
+              return res.send({
+                success : false,
+                message : "User already is a member of the team"
+              });
+          } else if(users[0].teamInvites.includes(teamId)) {
+              return res.send({
+                success : false,  
+                message : "User is already invited"
+              });
+          } else {
+              users[0].teamInvites.push(teamId);
+              users[0].save(err => {
+                if(err) {
+                    return res.send({
+                    success : false,
+                    message : "Error: Server error"
+                    });
+                } else {
+                    return res.send({
+                      success : true,
+                      message : "Invitation to the team sent"
+                    });
+                }
+              });
+          }
+        });
     }
   });
+
 });
 
+router.get('/invites', (req, res, next) => {
+  const { query } = req;
+  const { token } = query;
+  
+  UserSession.find({
+      _id : token,
+      isDeleted : false 
+  }, (err, sessions) => {
+      if(err) {
+          return res.send({
+          success : false,
+          message : 'Error : Server error'
+          });
+      }
+      if(sessions.length !== 1) {
+          return res.send({
+          success : false,
+          message : 'Error : Invalid'
+          });
+      } else {
+          User.find({
+            _id : sessions[0].userId
+          }, (err,users) =>{
+            if(err) {
+                return res.send({
+                success : false,
+                message : 'Error : Server error'
+                });
+            }
+            if(sessions.length != 1) {
+                return res.send({
+                success : false,
+                message : 'Error : Invalid'
+                });
+            } else {
+              return res.send({
+                success : true,
+                message : "User teams invites sent from sever",
+                teamInvites : users[0].teamInvites
+              });
+            }
+          });
+      }
+  });
+
+});
 module.exports = router;
