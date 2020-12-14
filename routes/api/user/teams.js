9eda0c8fa58dba9dc5a5f58433256108ad2b7452
tomaszsojka <tomek.s@windowslive.com>
2020-12-15 00:08:06 +0100

    Add team: saves name trimed and without multispaces, find compares case-insensitively

--- routes/api/user/teams.js
+++ routes/api/user/teams.js
@@ -6,69 +6,72 @@ const UserSession = require('../../../models/UserSession');
 
 router.post('/add-team', (req, res) => {
     const { body } = req;
-  const {
+    const {
       token,
       name,
       location
     } = body;
+    //remove whitespaces from the start and back and make inside whitespaces single
+    const trimNoMultipleSpaceName = name.trim().replace(/\s+/g, ' ')
 
-  Team.find({
-    name : name  
-  }, (err, previousTeams) => {
-    if(err) {
-      return res.send({
-        success : false,
-        message : "Error: Server error"
-      });
-    } else if(previousTeams.length > 0){
-      return res.send({
-        success : false,
-        message : "A team with the given name exists."
-      }); 
-    } else {
-        //no team with given name 
-        UserSession.find({
-            _id : token,
-            isDeleted : false 
-        }, (err, sessions) => {
-                if(err) {
-                    return res.send({
-                    success : false,
-                    message : 'Error : Server error'
-                    });
-                }
-                if(sessions.length != 1) {
-                    return res.send({
-                    success : false,
-                    message : 'Error : Invalid'
-                    });
-                } else {
-                    // proper UserSession found (of the same id as from local storage)
-                    
-                    //Save new team with UserSession's userId as captain
-                    const newTeam = new Team();
-                    newTeam.name = name;
-                    newTeam.location = location;
-                    newTeam.captainId = sessions[0].userId;
-                    newTeam.playersIds.push(sessions[0].userId);
-                    console.log("Saving new team");
-                    newTeam.save((err, team) => {
-                        if(err) {
-                            return res.send({
-                            success : false,
-                            message : "Error: Server error"
-                            });
-                        } else {
-                            return res.send({
-                            success : true,
-                            message : "Team added"
-                            });
-                        }
-                    });
-                }
+    Team.find({
+        // regex option i makes find case insensitive
+        name : { $regex: trimNoMultipleSpaceName, $options: "i" }
+    }, (err, previousTeams) => {
+        if(err) {
+        return res.send({
+            success : false,
+            message : "Error: Server error"
         });
-    }
-  });
+        } else if(previousTeams.length > 0){
+        return res.send({
+            success : false,
+            message : "A team with the given name exists."
+        }); 
+        } else {
+            //no team with given name 
+            UserSession.find({
+                _id : token,
+                isDeleted : false 
+            }, (err, sessions) => {
+                    if(err) {
+                        return res.send({
+                        success : false,
+                        message : 'Error : Server error'
+                        });
+                    }
+                    if(sessions.length != 1) {
+                        return res.send({
+                        success : false,
+                        message : 'Error : Invalid'
+                        });
+                    } else {
+                        // proper UserSession found (of the same id as from local storage)
+                        
+                        //Save new team with UserSession's userId as captain
+                        const newTeam = new Team();
+                        newTeam.name = trimNoMultipleSpaceName;
+                        newTeam.location = location;
+                        newTeam.captainId = sessions[0].userId;
+                        newTeam.playersIds.push(sessions[0].userId);
+                        console.log("Saving new team");
+                        newTeam.save((err, team) => {
+                            if(err) {
+                                return res.send({
+                                success : false,
+                                message : "Error: Server error"
+                                });
+                            } else {
+                                return res.send({
+                                success : true,
+                                message : "Team added"
+                                });
+                            }
+                        });
+                    }
+            });
+        }
+    });
 });
 
 
